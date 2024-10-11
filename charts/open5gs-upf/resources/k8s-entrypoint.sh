d0c3442e4c3c950c7a7918c093f44b1dd3119328
sgrannec <77071609+sgrannec@users.noreply.github.com>
2024-10-11 22:13:26 +0200

    Update k8s-entrypoint.sh

--- charts/open5gs-upf/resources/k8s-entrypoint.sh
+++ charts/open5gs-upf/resources/k8s-entrypoint.sh
@@ -2,6 +2,8 @@
 set -e
 
 echo "Executing k8s customized entrypoint.sh"
+echo "Creating net device ogstun"
+ip tuntap del ogstun mode tun
 
 {{- range .Values.config.subnetList }}
 {{- if .createDev }}
@@ -19,6 +21,7 @@ sysctl -w net.ipv4.ip_forward=1;
 {{- if .enableNAT }}
 echo "Enable NAT for {{ .subnet }} and device {{ .dev }}"
 iptables -t nat -A POSTROUTING -s {{ .subnet }} ! -o {{ .dev }} -j MASQUERADE;
+iptables -I FORWARD -i ogstun -j ACCEPT;
 {{- end }}
 {{- end }}
 {{- end }}
